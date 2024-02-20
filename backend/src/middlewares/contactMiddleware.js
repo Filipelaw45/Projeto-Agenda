d773b6a77bddbc7bf5ed0881168c96c2c8abcfaf
Filipelaw45 <filipe.law@gmail.com>
2024-02-20 08:43:36 -0300

    refactor: trocado error por message text

--- backend/src/middlewares/contactMiddleware.js
+++ backend/src/middlewares/contactMiddleware.js
@@ -4,26 +4,26 @@ const validateUserSingUp = (req, res, next) => {
     const { name, phoneNumber } = req.body;
 
     if (!name || !phoneNumber) {
-      return res.status(400).json({ error: 'Nome e numeros são requeridos' });
+      return res.status(400).json({ message: 'Nome e numeros são requeridos' });
     }
 
     if (typeof name !== 'string' || typeof phoneNumber !== 'string') {
-      return res.status(400).json({ error: 'Nome e número de telefone devem ser strings' });
+      return res.status(400).json({ message: 'Nome e número de telefone devem ser strings' });
     }
 
     if (name.length < 3 || name.length > 50) {
-      return res.status(400).json({ error: 'O nome deve ter entre 3 e 50 caracteres' });
+      return res.status(400).json({ message: 'O nome deve ter entre 3 e 50 caracteres' });
     }
 
     const phoneRegex = /^\d{10,11}$/;
     if (!phoneNumber.match(phoneRegex)) {
-      return res.status(400).json({ error: 'Formato de número de telefone inválido. Deve ter 10 ou 11 dígitos' });
+      return res.status(400).json({ message: 'Formato de número de telefone inválido. Deve ter 10 ou 11 dígitos' });
     }
 
     next();
   } catch (error) {
     console.error(error);
-    return res.status(500).json({ error: 'Internal server error' });
+    return res.status(500).json({ message: 'Internal server error' });
   }
 };
 
